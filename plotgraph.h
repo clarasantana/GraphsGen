0ac764a3dab031cb9bc74abfc4c6973b59b1b545
Maria Clara Santana <claarasantana@icloud.com>
2014-12-12 04:10:37 -0300

    Create plotgraph.h

---
+++ plotgraph.h
@@ -0,0 +1,6 @@
+/* This file was automatically generated.  Do not edit! */
+#include <cairo/cairo.h>
+void draw_Axis(cairo_t *context,int widht,int height);
+void draw_Backgroud(cairo_t *context,int width,int height);
+void plot_Point(cairo_t *context, int x, int y, int colors[]);
+void show_Points(cairo_t *context, int x[], int y[], int colors[], double scale);
